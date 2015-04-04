5affd14c0f100a19546f58a87d588fd86e0dab1f
jacobpadkins <jpadkins@smu.edu>
2015-04-04 04:54:10 -0500

    added first smooth turn

---
+++ OculusTest/Assets/Scripts/Rotate_Camera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotateX = Random.Range (0, 50);
+		transform.eulerAngles = new Vector3(0,0,rotateX);
+	}
+}
