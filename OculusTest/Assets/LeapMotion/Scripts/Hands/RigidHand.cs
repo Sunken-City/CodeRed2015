c4c0863f0cb09392ba4f6e91a74a1126d80bb071
picoriley <acloudy@mail.smu.edu>
2015-04-03 23:24:45 -0500

    We fire ball lasers now

--- OculusTest/Assets/LeapMotion/Scripts/Hands/RigidHand.cs
+++ OculusTest/Assets/LeapMotion/Scripts/Hands/RigidHand.cs
@@ -106,9 +106,9 @@ public class RigidHand : SkeletalHand
         }
         if (Time.time - lastFireTime > reloadTime)
         {
-            Finger finger = this.hand_.Fingers.Frontmost;
+            Finger finger = this.hand_.Fingers[1];
             Vector3 normal = finger.Direction.ToUnity();
-            GameObject bullet = Instantiate(projectile, fingers[1].GetTipPosition(), Quaternion.identity) as GameObject;
+            GameObject bullet = Instantiate(projectile, fingers[1].GetTipPosition() + normal / 2, Quaternion.identity) as GameObject;
             bullet.GetComponent<Rigidbody>().velocity = normal * projectileSpeed;
             lastFireTime = Time.time;
         }
