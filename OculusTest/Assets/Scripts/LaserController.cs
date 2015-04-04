bf0f2831ce668d1865111618a7c76d88fc49f962
picoriley <acloudy@mail.smu.edu>
2015-04-04 00:02:09 -0500

    Replaced the index finger with a laser

---
+++ OculusTest/Assets/Scripts/LaserController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserController : MonoBehaviour
+{
+    public float minimum = 0.2f;
+    public float maximum = 0.3f;
+
+    private LineRenderer renderer;
+    private float currentWidth;
+    private float length;
+
+    // Use this for initialization
+    void Start()
+    {
+        renderer = this.GetComponent<LineRenderer>();
+        length = maximum - minimum;
+        currentWidth = minimum + (length / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentWidth = minimum + Mathf.PingPong(Time.time, length);
+        renderer.SetWidth(currentWidth, currentWidth);
+    }
+}
