1a3d4dcf4a95ebed0b8bf794bb734296704b1074
Ethan Busbee <ethan.busbee@gmail.com>
2015-04-04 07:32:44 -0500

    Migrated Camera_Move.cs from Rails
    Copied over the camera movement script from the Rails branch. I tried to
    merge the branches, but GitHub for Windows was all "nah dawg fuck dat
    noise"

--- OculusTest/Assets/Scripts/Camera_Move.cs
+++ OculusTest/Assets/Scripts/Camera_Move.cs
@@ -3,40 +3,53 @@ using System.Collections;
 
 public class Camera_Move : MonoBehaviour
 {
-
-    public float spd;
-    public GameObject objectName;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveTowardsCoordinates();
-    }
-    private void MoveTowardsCoordinates()
-    {
-        float speed = spd;
-
-        //GameObject player = GameObject.Find (objectName);
-        //Transform playerTransform = player.transform;
-
-        //Vector3 targetPosition = new Vector3(x,y,z);
-        Vector3 targetPosition = objectName.transform.position;
-        Vector3 currentPosition = this.transform.position;
-        if (Vector3.Distance(currentPosition, targetPosition) > .1f)
-        {
-            Vector3 directionOfTravel = targetPosition - currentPosition;
-            directionOfTravel.Normalize();
-            this.transform.Translate(
-                (directionOfTravel.x * speed * Time.deltaTime),
-                (directionOfTravel.y * speed * Time.deltaTime),
-                (directionOfTravel.z * speed * Time.deltaTime),
-                Space.World);
-        }
-    }
+	
+	public float spd;
+	public GameObject[] gObject;
+	private int targetIndex = 0;
+	
+	private GameObject currentTarget;
+	
+	// Use this for initialization
+	void Start()
+	{
+		currentTarget = gObject [targetIndex];
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		MoveTowardsCoordinates();
+	}
+	
+	public void nextDestination() {
+		targetIndex++;
+		currentTarget = gObject [targetIndex];
+		Debug.Log("Targeting destination " + (targetIndex + 1));
+	}
+	
+	private void MoveTowardsCoordinates()
+	{
+		float speed = spd;
+		
+		//GameObject player = GameObject.Find (objectName);
+		//Transform playerTransform = player.transform;
+		
+		//Vector3 targetPosition = new Vector3(x,y,z);
+		Vector3 targetPosition = currentTarget.transform.position;
+		Vector3 currentPosition = this.transform.position;
+		if (Vector3.Distance(currentPosition, targetPosition) > 1f)
+		{
+			Vector3 directionOfTravel = targetPosition - currentPosition;
+			directionOfTravel.Normalize();
+			this.transform.Translate(
+				(directionOfTravel.x * speed * Time.deltaTime),
+				(directionOfTravel.y * speed * Time.deltaTime),
+				(directionOfTravel.z * speed * Time.deltaTime),
+				Space.World);
+		}
+		else {
+			nextDestination();
+		}
+	}
 }
