94ce6b2a1b71c1d5b298b6ba4a991dd05b6dac80
picoriley <acloudy@mail.smu.edu>
2015-04-03 21:42:40 -0500

    Projectile is spawning and firing!

--- OculusTest/Assets/LeapMotion/Scripts/Hands/RigidHand.cs
+++ OculusTest/Assets/LeapMotion/Scripts/Hands/RigidHand.cs
@@ -9,83 +9,106 @@ using System.Collections;
 using Leap;
 
 // The model for our rigid hand made out of various polyhedra.
-public class RigidHand : SkeletalHand {
+public class RigidHand : SkeletalHand
+{
 
-  public float filtering = 0.5f;
+    public float filtering = 0.5f;
+    public GameObject projectile;
+    public int projectileSpeed = 2;
 
-  void Start() {
-    palm.GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
-    Leap.Utils.IgnoreCollisions(gameObject, gameObject);
-  }
-
-  public override void InitHand() {
-    base.InitHand();
-  }
-
-  public override void UpdateHand() {
-    for (int f = 0; f < fingers.Length; ++f) {
-      if (fingers[f] != null)
-        fingers[f].UpdateFinger();
+    private float lastFireTime;
+    void Start()
+    {
+        palm.GetComponent<Rigidbody>().maxAngularVelocity = Mathf.Infinity;
+        Leap.Utils.IgnoreCollisions(gameObject, gameObject);
+        lastFireTime = Time.time;
     }
 
-    if (palm != null) {
-      // Set palm velocity.
-      Vector3 target_position = GetPalmCenter();
-      palm.GetComponent<Rigidbody>().velocity = (target_position - palm.transform.position) *
-                                (1 - filtering) / Time.deltaTime;
-
-      // Set palm angular velocity.
-      Quaternion target_rotation = GetPalmRotation();
-      Quaternion delta_rotation = target_rotation *
-                                  Quaternion.Inverse(palm.transform.rotation);
-      float angle = 0.0f;
-      Vector3 axis = Vector3.zero;
-      delta_rotation.ToAngleAxis(out angle, out axis);
-
-      if (angle >= 180) {
-        angle = 360 - angle;
-        axis = -axis;
-      }
-      if (angle != 0) {
-        float delta_radians = (1 - filtering) * angle * Mathf.Deg2Rad;
-        palm.GetComponent<Rigidbody>().angularVelocity = delta_radians * axis / Time.deltaTime;
-      }
+    public override void InitHand()
+    {
+        base.InitHand();
     }
 
-    if (forearm != null) {
-      // Set arm dimensions.
-      CapsuleCollider capsule = forearm.GetComponent<CapsuleCollider> ();
-      if (capsule != null) {
-        // Initialization
-        capsule.direction = 2;
-        forearm.transform.localScale = new Vector3(1f, 1f, 1f);
-        
-        // Update
-        capsule.radius = GetArmWidth () / 2f;
-        capsule.height = GetArmLength () + GetArmWidth ();
-      }
+    public override void UpdateHand()
+    {
+        for (int f = 0; f < fingers.Length; ++f)
+        {
+            if (fingers[f] != null)
+                fingers[f].UpdateFinger();
+        }
 
-      // Set arm velocity.
-      Vector3 target_position = GetArmCenter ();
-      forearm.GetComponent<Rigidbody>().velocity = (target_position - forearm.transform.position) *
-        (1 - filtering) / Time.deltaTime;
+        if (palm != null)
+        {
+            // Set palm velocity.
+            Vector3 target_position = GetPalmCenter();
+            palm.GetComponent<Rigidbody>().velocity = (target_position - palm.transform.position) *
+                                      (1 - filtering) / Time.deltaTime;
 
-      // Set arm velocity.
-      Quaternion target_rotation = GetArmRotation ();
-      Quaternion delta_rotation = target_rotation *
-        Quaternion.Inverse (forearm.transform.rotation);
-      float angle = 0.0f;
-      Vector3 axis = Vector3.zero;
-      delta_rotation.ToAngleAxis (out angle, out axis);
+            // Set palm angular velocity.
+            Quaternion target_rotation = GetPalmRotation();
+            Quaternion delta_rotation = target_rotation *
+                                        Quaternion.Inverse(palm.transform.rotation);
+            float angle = 0.0f;
+            Vector3 axis = Vector3.zero;
+            delta_rotation.ToAngleAxis(out angle, out axis);
 
-      if (angle >= 180) {
-        angle = 360 - angle;
-        axis = -axis;
-      }
-      if (angle != 0) {
-        float delta_radians = (1 - filtering) * angle * Mathf.Deg2Rad;
-        forearm.GetComponent<Rigidbody>().angularVelocity = delta_radians * axis / Time.deltaTime;
-      }
+            if (angle >= 180)
+            {
+                angle = 360 - angle;
+                axis = -axis;
+            }
+            if (angle != 0)
+            {
+                float delta_radians = (1 - filtering) * angle * Mathf.Deg2Rad;
+                palm.GetComponent<Rigidbody>().angularVelocity = delta_radians * axis / Time.deltaTime;
+            }
+        }
+
+        if (forearm != null)
+        {
+            // Set arm dimensions.
+            CapsuleCollider capsule = forearm.GetComponent<CapsuleCollider>();
+            if (capsule != null)
+            {
+                // Initialization
+                capsule.direction = 2;
+                forearm.transform.localScale = new Vector3(1f, 1f, 1f);
+
+                // Update
+                capsule.radius = GetArmWidth() / 2f;
+                capsule.height = GetArmLength() + GetArmWidth();
+            }
+
+            // Set arm velocity.
+            Vector3 target_position = GetArmCenter();
+            forearm.GetComponent<Rigidbody>().velocity = (target_position - forearm.transform.position) *
+              (1 - filtering) / Time.deltaTime;
+
+            // Set arm velocity.
+            Quaternion target_rotation = GetArmRotation();
+            Quaternion delta_rotation = target_rotation *
+              Quaternion.Inverse(forearm.transform.rotation);
+            float angle = 0.0f;
+            Vector3 axis = Vector3.zero;
+            delta_rotation.ToAngleAxis(out angle, out axis);
+
+            if (angle >= 180)
+            {
+                angle = 360 - angle;
+                axis = -axis;
+            }
+            if (angle != 0)
+            {
+                float delta_radians = (1 - filtering) * angle * Mathf.Deg2Rad;
+                forearm.GetComponent<Rigidbody>().angularVelocity = delta_radians * axis / Time.deltaTime;
+            }
+        }
+        if (Time.time - lastFireTime > 0.5f)
+        {
+            Vector3 normal = this.hand_.PalmNormal.ToUnity();
+            GameObject bullet = Instantiate(projectile, palm.transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().velocity = normal * projectileSpeed;
+            lastFireTime = Time.time;
+        }
     }
-  }
 }
