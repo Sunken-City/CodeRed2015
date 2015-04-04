7ef88164ac18ab3f1b7bbc3a34ff872ab9b75042
jacobpadkins <jpadkins@smu.edu>
2015-04-04 08:39:51 -0500

    Added tint code, called Enemy_Basic_...

---
+++ OculusTest/Assets/Scripts/Enemy_Basic_Functions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Basic_Functions : MonoBehaviour {
+
+	public int x, y, z;
+	Color healthHue;
+
+	// Use this for initialization
+	void Start () {
+		healthHue = gameObject.GetComponent<Renderer>().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hurtMe ();
+	}
+
+	public void hurtMe() {
+		healthHue.r = healthHue.r + 1;
+		gameObject.GetComponent<Renderer>().material.SetColor("_Color", (healthHue));
+	}
+}
