e880a843f98b98d8c8f3916222b380ee68f3ab05
picoriley <acloudy@mail.smu.edu>
2015-04-04 10:56:03 -0500

    Gambling with the prefab-breaking issue. Running an experiment

---
+++ Egypt/Assets/Scripts/Enemy_Basic_Functions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Basic_Functions : MonoBehaviour
+{
+    public float healthLossMultiplier = 0.01f;
+    private Color healthHue;
+
+    // Use this for initialization
+    void Start()
+    {
+        healthHue = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void hurtMe()
+    {
+        healthHue.r += healthLossMultiplier;
+        print(healthHue.r);
+        gameObject.GetComponent<Renderer>().material.SetColor ("_TintColor", healthHue);
+        if (healthHue.r >= 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Egypt/Assets/Scripts/dieAfterTime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dieAfterTime : MonoBehaviour
+{
+    public float lifetime = 10.0f;
+    private float birthday;
+    // Use this for initialization
+    void Start()
+    {
+        birthday = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - birthday > lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
