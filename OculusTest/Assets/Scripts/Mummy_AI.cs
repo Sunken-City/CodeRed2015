0897f4909fe0a56c8233063d77118595ccbbc421
jacobpadkins <jpadkins@smu.edu>
2015-04-04 10:29:14 -0500

    Added some AI and collision scripts

---
+++ OculusTest/Assets/Scripts/Mummy_AI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mummy_AI : MonoBehaviour {
+
+	bool canShoot = false;
+	public int secondsWait = 4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canShoot) {
+			// fire projectile
+
+			//
+			canShoot = false;
+		} 
+		else {
+			WaitForSeconds(secondsWait);
+			canShoot = true;
+		}
+	}
+}
---
+++ OculusTest/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Script : MonoBehaviour {
+
+	public int health = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Sphere") {
+			health = health - 5;
+			Destroy(col.gameObject);
+		}
+	}
+}
