8debce5a8c8286ce2d9bc8e9a6a0f24f0f4a5c0c
jacobpadkins <jpadkins@smu.edu>
2015-04-04 03:37:37 -0500

    trying to get destination switching to work

--- OculusTest/Assets/Scripts/Camera_Move.cs
+++ OculusTest/Assets/Scripts/Camera_Move.cs
@@ -5,7 +5,7 @@ public class Camera_Move : MonoBehaviour
 {
 
     public float spd;
-    public GameObject objectName;
+    public GameObject gObject;
 
     // Use this for initialization
     void Start()
@@ -18,6 +18,11 @@ public class Camera_Move : MonoBehaviour
     {
         MoveTowardsCoordinates();
     }
+
+	public void setDestination(string s) {
+		gObject = GameObject.Find(s);
+	}
+
     private void MoveTowardsCoordinates()
     {
         float speed = spd;
@@ -26,7 +31,7 @@ public class Camera_Move : MonoBehaviour
         //Transform playerTransform = player.transform;
 
         //Vector3 targetPosition = new Vector3(x,y,z);
-        Vector3 targetPosition = objectName.transform.position;
+        Vector3 targetPosition = gObject.transform.position;
         Vector3 currentPosition = this.transform.position;
         if (Vector3.Distance(currentPosition, targetPosition) > .1f)
         {
